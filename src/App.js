a28264616a866988692ce6d7e29e734591a1699c
Ronnie Villarini <Villarini91@gmail.com>
2020-07-18 18:40:47 -0500

    add react router, add menu item navigation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Switch, Route } from "react-router-dom";
 
 import Homepage from './pages/homepage/homepage'
 
@@ -7,7 +8,9 @@ import './App.css';
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+      </Switch>
     </div>
   );
 }
