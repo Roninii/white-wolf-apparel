dcb45194140d8a8cbf11d192b69bd6d04be52573
Ronnie Villarini <Villarini91@gmail.com>
2020-07-18 22:13:33 -0500

    add sign up component boilerplate

---
+++ src/components/sign-up/sign-up.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./sign-up.scss";
+
+export default class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+}
