acac954a980ba4001ef2e00cd0d76d54ca61f2d5
Ronnie Villarini <Villarini91@gmail.com>
2020-07-18 21:05:53 -0500

    add nav and branding

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { Switch, Route } from "react-router-dom";
 import './App.css';
 import ShopPage from './pages/shop/shoppage';
 import Homepage from './pages/homepage/homepage'
+import Header from './components/header/header';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route path='/shop' component={ShopPage} />
---
+++ src/components/header/header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { ReactComponent as Logo } from "../../assets/logo.svg";
+
+import "./header.scss";
+
+const Header = () => (
+  <header className="header">
+    <Link className="logo-container" to="/">
+      <Logo className="logo" />
+    </Link>
+    <nav className="options">
+      <Link className="option" to="/shop">
+        Shop
+      </Link>
+      <Link className="option" to="/contact">
+        Contact
+      </Link>
+    </nav>
+  </header>
+);
+
+export default Header;
