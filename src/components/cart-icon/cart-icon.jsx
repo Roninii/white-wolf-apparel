7050c56ccbdb0575f3f32d0495395c4ce7554773
Ronnie Villarini <Villarini91@gmail.com>
2020-07-24 07:41:42 -0500

    add reselct and create redux selectors

--- src/components/cart-icon/cart-icon.jsx
+++ src/components/cart-icon/cart-icon.jsx
@@ -1,24 +1,25 @@
-import React from "react";
-import { connect } from "react-redux";
+import React from 'react';
+import { connect } from 'react-redux';
 
-import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
+import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
-import "./cart-icon.scss";
-import { toggleCartHidden } from "../../redux/cart/cart.actions";
+import './cart-icon.scss';
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
-const CartIcon = ({ toggleCartHidden, cartItems }) => (
-  <button className="cart-icon" onClick={toggleCartHidden}>
-    <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">{cartItems.length}</span>
-  </button>
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
+    <button className='cart-icon' onClick={toggleCartHidden}>
+        <ShoppingIcon className='shopping-icon' />
+        <span className='item-count'>{itemCount}</span>
+    </button>
 );
 
 const mapDispatchToProps = (dispatch) => ({
-  toggleCartHidden: () => dispatch(toggleCartHidden()),
+    toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems,
+const mapStateToProps = (state) => ({
+    itemCount: selectCartItemsCount(state),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
--- src/components/cart/cart.jsx
+++ src/components/cart/cart.jsx
@@ -1,27 +1,28 @@
-import React from "react";
-import { connect } from "react-redux";
+import React from 'react';
+import { connect } from 'react-redux';
 
-import "./cart.scss";
-import BaseButton from "../base/button/base-button";
-import CartItem from "../cart-item/cart-item";
+import './cart.scss';
+import BaseButton from '../base/button/base-button';
+import CartItem from '../cart-item/cart-item';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 
 const Cart = ({ cartItems }) => (
-  <aside className="cart-dropdown">
-    <section className="cart-items">
-      <ul className="item-list">
-        {cartItems.map((item) => (
-          <li key={item.id}>
-            <CartItem item={item} />
-          </li>
-        ))}
-      </ul>
-    </section>
-    <BaseButton>Go To Checkout</BaseButton>
-  </aside>
+    <aside className='cart-dropdown'>
+        <section className='cart-items'>
+            <ul className='item-list'>
+                {cartItems.map((item) => (
+                    <li key={item.id}>
+                        <CartItem item={item} />
+                    </li>
+                ))}
+            </ul>
+        </section>
+        <BaseButton>Go To Checkout</BaseButton>
+    </aside>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems,
+const mapStateToProps = (state) => ({
+    cartItems: selectCartItems(state),
 });
 
 export default connect(mapStateToProps)(Cart);
