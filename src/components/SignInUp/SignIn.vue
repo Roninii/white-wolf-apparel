16b330b74fd51627d649e6c55928784d9c8c6842
Ronnie Villarini <Villarini91@gmail.com>
2020-07-23 22:16:39 -0500

    add sign in component

---
+++ src/components/SignInUp/SignIn.vue
@@ -0,0 +1,77 @@
+<template>
+    <article class="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password.</span>
+
+        <form @submit.prevent="handleSubmit">
+            <BaseInput
+                id="email"
+                type="email"
+                name="email"
+                required
+                v-model="email"
+                label="email"
+            />
+
+            <BaseInput
+                id="password"
+                type="password"
+                name="password"
+                required
+                v-model="password"
+                label="password"
+            />
+
+            <section class="button-group">
+                <BaseButton type="submit">
+                    Sign In
+                </BaseButton>
+
+                <BaseButton @click.prevent="signInWithGoogle" isGoogleSignIn>
+                    Sign in with Google
+                </BaseButton>
+            </section>
+        </form>
+    </article>
+</template>
+
+<script>
+import BaseButton from '@/components/Base/BaseButton';
+import BaseInput from '@/components/Base/BaseInput';
+
+export default {
+    name: 'SignIn',
+    components: {
+        BaseButton,
+        BaseInput,
+    },
+    data() {
+        return { email: '', password: '' };
+    },
+    methods: {
+        handleSubmit() {},
+        signInWithGoogle() {
+            console.log('test');
+        },
+    },
+};
+</script>
+
+<style lang="postcss" scoped>
+.sign-in {
+    width: 380px;
+    display: flex;
+    flex-direction: column;
+
+    & .title {
+        margin: 10px 0;
+    }
+
+    & .button-group {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-gap: 0.3rem;
+        gap: 0.3rem;
+    }
+}
+</style>
