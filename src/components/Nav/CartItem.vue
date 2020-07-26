8792c431f8f650f4548361115e8f54f2eff53a71
Ronnie Villarini <Villarini91@gmail.com>
2020-07-25 22:47:40 -0500

    allow adding items to cart

---
+++ src/components/Nav/CartItem.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="cart-item">
+    <img :src="item.imageUrl" :alt="`model wearning ${item.name}`" />
+    <div class="item-details">
+      <span class="name">{{ item.name }}</span>
+      <span class="price"> {{ item.quantity }} x ${{ item.price }} </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CartItem",
+  props: {
+    item: {
+      type: Object,
+      default: () => {},
+    },
+  },
+};
+</script>
+
+<style lang="postcss" scoped>
+.cart-item {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+
+  & img {
+    width: 12rem;
+    height: auto;
+  }
+
+  & .item-details {
+    width: 70%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    padding: 10px 20px;
+
+    & .name {
+      font-size: 16px;
+    }
+  }
+}
+</style>
