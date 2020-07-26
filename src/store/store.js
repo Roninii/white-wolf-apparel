40c909eddd83332c855b1e7066ae0cb6ebafd6f9
Ronnie Villarini <Villarini91@gmail.com>
2020-07-25 21:46:34 -0500

    namespace user vuex state, actions, and mutations

--- src/store/store.js
+++ src/store/store.js
@@ -1,26 +1,21 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
-import router from '@/router/index.js'
+// modules
+import * as user from '@/store/modules/user'
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
+    modules: {
+        user
+    },
     state: {
-        currentUser: null,
     },
     mutations: {
-        SET_CURRENT_USER(state, user) {
-            state.currentUser = user;
-        }
+
     },
     actions: {
-        updateCurrentUser({commit, dispatch}, user) {
-            commit('SET_CURRENT_USER', user);
-            if(user) dispatch('redirectUser')
-        },
-        redirectUser() {
-            router.push('/')
-        }
     }
+
 });
