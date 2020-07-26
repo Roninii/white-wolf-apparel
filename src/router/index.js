fc6d59882679d13f4f278606867e99b9d064a854
Ronnie Villarini <Villarini91@gmail.com>
2020-07-25 21:31:31 -0500

    link directories to shop page

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ Vue.use(VueRouter)
     component: Home
     },
     {
-      path: '/shop',
+      path: '/shop/:category',
       name: 'shop',
       component: () => import(/* webpackChunkName: "shop" */ '../views/Shop.vue'),
     },
